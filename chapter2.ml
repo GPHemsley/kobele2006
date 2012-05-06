b57b374ee74707aa80030ee489a9436ccd527337
Gordon P. Hemsley <me@gphemsley.org>
2012-05-06 13:02:43 -0400

    Add more derivation tests.

--- chapter2.ml
+++ chapter2.ml
@@ -62,12 +62,12 @@ let merge_selection ( stree : subtree * subtree list ) ( ctree : subtree * subtr
     let st_f = List.tl (st_merge.features) in
     let ct_f = List.tl (ct_merge.features) in
     match ct_f with
-          []    ->print_endline "no more features";
+          []    ->(*print_endline "no more features";*)
             (
                 (Derivation.make_subtree [] st_merge.head ( ct_merge.spec @ ct_merge.head @ ct_merge.comp ) st_f),
                 ct_move
             )
-        | _::_  ->print_endline "licensing features";
+        | _::_  ->(*print_endline "licensing features";*)
             (
                 (Derivation.make_subtree st_merge.spec st_merge.head st_merge.comp st_f),
                 st_move @ [ (Derivation.make_subtree ct_merge.spec ct_merge.head ct_merge.comp ct_f) ]
@@ -145,12 +145,12 @@ let check_licensee licensor licensee =
     let f_or = List.tl licensor.features in
     let f_ee = List.tl ee1.features in
     match f_ee with
-          []    ->print_endline "no more licensing features";
+          []    ->(*print_endline "no more licensing features";*)
             (
                 (Derivation.make_subtree ( ee1.spec @ ee1.head @ ee1.comp ) licensor.head licensor.comp f_or),
                 (List.tl licensee)
             )
-        | _::_  ->print_endline "more licensing features";
+        | _::_  ->(*print_endline "more licensing features";*)
             (
                 (Derivation.make_subtree licensor.spec licensor.head licensor.comp f_or),
                 (Derivation.make_subtree ee1.spec ee1.head ee1.comp f_ee)::(List.tl licensee)
@@ -281,13 +281,26 @@ let _ointment_ = List.hd (en_get_entries_for "ointment");;
 let _devour_ = List.hd (en_get_entries_for "devour");;
 let _arrive_ = List.hd (en_get_entries_for "arrive");;
 
-let _will_ = List.hd (en_get_entries_for "will");;
-let __ed_ = List.hd (en_get_entries_for "-ed");;
-let __ing_ = List.hd (en_get_entries_for "-ing");;
+let _seem_ = List.hd (en_get_entries_for "seem");;
 
-let __act__ = List.nth (en_get_entries_for "") 0;;
-let __perf__ = List.nth (en_get_entries_for "") 1;;
-let __prog__ = List.nth (en_get_entries_for "") 2;;
+let _will_ = List.hd (en_get_entries_for "will");;
+let __s_ = List.hd (en_get_entries_for "-s");;
+let __ed_ = List.hd (en_get_entries_for "-ed");;
+
+let _have__PERF__ = List.nth (en_get_entries_for "have") 0;;
+let __en__PERF__ = List.nth (en_get_entries_for "-en") 1;;
+let __PERF__ = List.nth (en_get_entries_for "") 1;;
+
+let _be__PROG__ = List.nth (en_get_entries_for "be") 0;;
+let __ing_ = List.hd (en_get_entries_for "-ing");;
+let __PROG__ = List.nth (en_get_entries_for "") 2;;
+
+let _be__PASS__ = List.nth (en_get_entries_for "be") 1;;
+let __en__PASS__ = List.nth (en_get_entries_for "-en") 0;;
+let __ACT__ = List.nth (en_get_entries_for "") 0;;
+
+let _to__INF__ = List.nth (en_get_entries_for "to") 0;;
+let _that__COMP__ = List.nth (en_get_entries_for "that") 0;;
 
 let _promise_ = List.nth (en_get_entries_for "promise") 1;;
 
@@ -309,17 +322,70 @@ let _John's_ointment_ =
     d4
 ;;
 
-(* Page 57 *)
+(* Page 22 *)
+(* (2.7) John will devour the ointment. *)
+let _John_will_devour_the_ointment_ =
+    let d1 = merge _the_ _ointment_ in
+    let d2 = merge _devour_ d1 in
+    let d3 = merge __ACT__ d2 in
+    let d4 = move d3 in
+    let d5 = merge _John_ d4 in
+    let d6 = move d5 in
+    let d7 = merge __PROG__ d6 in
+    let d8 = merge __PERF__ d7 in
+    let d9 = merge _will_ d8 in
+    let d10 = move d9 in
+    let d11 = move d10 in
+    d11
+;;
+
+(* Page 22, 28 *)
+(* (2.8) John is devouring the ointment. *)
+let _John_is_devouring_the_ointment_ =
+    let d1 = merge _the_ _ointment_ in
+    let d2 = merge _devour_ d1 in
+    let d3 = merge __ACT__ d2 in
+    let d4 = move d3 in
+    let d5 = merge _John_ d4 in
+    let d6 = move d5 in
+    let d7 = merge __ing_ d6 in
+    let d8 = merge _be__PROG__ d7 in
+    let d9 = merge __PERF__ d8 in
+    let d10 = merge __s_ d9 in
+    let d11 = move d10 in
+    let d12 = move d11 in
+    d12
+;;
+
+(* Page 24 *)
+(* (2.17) John will be devouring the ointment. *)
+let _John_will_be_devouring_the_ointment_ =
+    let d1 = merge _the_ _ointment_ in
+    let d2 = merge _devour_ d1 in
+    let d3 = merge __ACT__ d2 in
+    let d4 = move d3 in
+    let d5 = merge _John_ d4 in
+    let d6 = move d5 in
+    let d7 = merge __ing_ d6 in
+    let d8 = merge _be__PROG__ d7 in
+    let d9 = merge __PERF__ d8 in
+    let d10 = merge _will_ d9 in
+    let d11 = move d10 in
+    let d12 = move d11 in
+    d12
+;;
+
+(* Page 22, 57 *)
 (* John devoured the ointment. *)
 let _John_devoured_the_ointment_ =
     let d1 = merge _the_ _ointment_ in
     let d2 = merge _devour_ d1 in
-    let d3 = merge __act__ d2 in
+    let d3 = merge __ACT__ d2 in
     let d4 = move d3 in
     let d5 = merge _John_ d4 in
     let d6 = move d5 in
-    let d7 = merge __prog__ d6 in
-    let d8 = merge __perf__ d7 in
+    let d7 = merge __PROG__ d6 in
+    let d8 = merge __PERF__ d7 in
     let d9 = merge __ed_ d8 in
     let d10 = move d9 in
     let d11 = move d10 in
@@ -330,14 +396,35 @@ let _John_devoured_the_ointment_ =
 (* (2.50) Something devoured everyone. *)
 let _something_devoured_everyone_ =
     let d1 = merge _devour_ _everyone_ in
-    let d2 = merge __act__ d1 in
+    let d2 = merge __ACT__ d1 in
     let d3 = move d2 in
-    let d4 = merge d3 _something_ in
+    let d4 = merge _something_ d3 in
     let d5 = move d4 in
-    let d6 = merge __prog__ d5 in
-    let d7 = merge __perf__ d6 in
+    let d6 = merge __PROG__ d5 in
+    let d7 = merge __PERF__ d6 in
     let d8 = merge __ed_ d7 in
     let d9 = move d8 in
     let d10 = move d9 in
     d10
+;;
+
+(* Page 82 *)
+(* (2.51) Something seems to be devouring everyone. *)
+let _something_seems_to_be_devouring_everyone_ =
+    let d1 = merge _devour_ _everyone_ in
+    let d2 = merge __ACT__ d1 in
+    let d3 = move d2 in
+    let d4 = merge _something_ d3 in
+    let d5 = move d4 in
+    let d6 = merge __ing_ d5 in
+    let d7 = merge _be__PROG__ d6 in
+    let d8 = merge __PERF__ d7 in
+    let d9 = merge _to__INF__ d8 in
+    let d10 = merge _seem_ d9 in
+    let d11 = merge __PROG__ d10 in
+    let d12 = merge __PERF__ d11 in
+    let d13 = merge __s_ d12 in
+    let d14 = move d13 in
+    let d15 = move d14 in
+    d15
 ;;
